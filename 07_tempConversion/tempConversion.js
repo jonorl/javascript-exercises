0626349b2e4442f5729db007d2c35e01fdd305a7
jonorl <39122230+jonorl@users.noreply.github.com>
2024-09-04 12:51:36 +0300

    Finished excercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  c = (f-32) * (5/9);
+  if(c % 1 != 0){
+    return parseFloat(c.toFixed(1));
+    }
+  else return c;
 };
 
-const convertToFahrenheit = function() {
-};
+const convertToFahrenheit = function(c) {
+  f = c * (9/5) + 32;
+  if(f % 1 != 0){
+    return parseFloat(f.toFixed(1));
+    }
+  else return f;
+  }
 
 // Do not edit below this line
 module.exports = {
