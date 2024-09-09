139093651e13e017cddbc9d939c3a1643a5c4804
jonorl <39122230+jonorl@users.noreply.github.com>
2024-09-09 09:58:32 +0300

    indent

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,11 +1,11 @@
 const reverseString = function(str) {
-let len = str.length;
-let reversedText = ""
-for (i = len; i !== 0; i--){
-    let reversedChar = str.at(i - len - 1);
-    reversedText += reversedChar;
-    }
-return reversedText;
+    let len = str.length;
+    let reversedText = ""
+    for (i = len; i !== 0; i--){
+        let reversedChar = str.at(i - len - 1);
+        reversedText += reversedChar;
+        }
+    return reversedText;
 };
 
 // Do not edit below this line
