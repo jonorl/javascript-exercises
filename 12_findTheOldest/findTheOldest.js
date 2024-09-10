df8dd31e59955b5d97e60881337efa8714fc6c05
jonorl <39122230+jonorl@users.noreply.github.com>
2024-09-10 15:05:42 +0300

    Completed excercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let newObject = {};
+    let maxYear = 0
+    let oldestPerson = ""
+    const currentYear = (new Date()).getFullYear();
+    for (person of people){
+        person.yearsLived = (person.yearOfDeath != undefined ? person.yearOfDeath : currentYear) - person.yearOfBirth;
+        if (person.yearsLived > maxYear){
+            maxYear = person.yearsLived
+            oldestPerson = person
+        };
+    }
+    people.push(newObject);
+    return oldestPerson;
 };
 
 // Do not edit below this line
