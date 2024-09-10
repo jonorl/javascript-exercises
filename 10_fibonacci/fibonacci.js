6591159e94665330fab1a4daf7607e565c19f09f
jonorl <39122230+jonorl@users.noreply.github.com>
2024-09-10 13:51:14 +0300

    Completed excercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
-
-};
-
+const fibonacci = function(num) {
+    if (num >= 0 && Number.isInteger(Number(num))){
+        let counter = []
+        for (let i = 0; i < (num + 1); i++){
+            if (counter.length === 0 || counter.length === 1){
+                counter.push(i);
+                }
+            else{
+                counter.push((counter[counter.length-1] + (counter[counter.length-2])));
+            }
+    
+        }  
+    return counter[num]
+    }
+    else {
+        return "OOPS"
+    }
+}
 // Do not edit below this line
 module.exports = fibonacci;
