34bd01fe4c7463fdec70ceb1013e7f9753c0d444
jonorl <39122230+jonorl@users.noreply.github.com>
2024-09-10 11:48:15 +0300

    Completed excercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+
+  let arr = []
+  arr = array
+
+  let summed = arr.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue},0
+  )
+  return summed;
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  let arr = []
+  arr = array
 
+  let multiplied = arr.reduce((accumulator, currentValue) => {
+    return accumulator * currentValue},1
+  )
+  return multiplied;
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return Math.pow(x, y)
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num < 0) return -1;
+  else if (num == 0) return 1;
+  else {
+    return (num * factorial(num - 1));
+  }
 };
 
 // Do not edit below this line
