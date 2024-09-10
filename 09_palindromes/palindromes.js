cdf4b0cc2e781b072cc017eedb918f4a5b2287c4
jonorl <39122230+jonorl@users.noreply.github.com>
2024-09-10 12:33:36 +0300

    Finished excercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const originalString = string.toLowerCase()
+                                 .split("")
+                                 .filter(val => val != "." && val != "!" && val != "," && val != " ")
+    const reversed =  originalString.slice()
+                                    .reverse();
+    return reversed.join("") === originalString.join("");
+    // return "reverse: " + reversed.join("") + " original: " + originalString.join("");
 };
 
 // Do not edit below this line
